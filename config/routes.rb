bf1660a4795681fccbc61b5bceb2a2777c7e521d
Gus King <gusssk@gmail.com>
2014-04-25 16:07:59 -0600

    Updated home and about pages with description of FreeHandi.
    * Updated README with github and heroku urls.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/handicap', to: 'handicap#index'
 
+  get '/about', to: 'handicap#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
